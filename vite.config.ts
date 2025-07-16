37a39766419149e8148a55309ed8df4b2334dd06
JiriResler <j.resler.29@gmail.com>
2025-07-16 15:23:46 +0200

    Add base path for serve command

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/solid-menu-manager/',
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/solid-menu-manager/',
+  }
+
+  if (command === 'serve') {
+    config.base = '/'
+  }
+
+  return config
 })
